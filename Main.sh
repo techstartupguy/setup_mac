50d5d3c1affabe7ae5b37c8088c5fd28ba73c77e
Gaurav <gg.spark@gmail.com>
2020-07-20 05:02:45 +0530

    Updated main

--- Main.sh
+++ Main.sh
@@ -1,45 +1,53 @@
-#!/bin/bash
-# Simple setup.sh for configuring Macintosh Workstation
+#!/usr/bin/env bash
+cd "$(dirname "$0")" || exit #Cd to project dir
+SECONDS=0
 
-clear
-echo "******************************************************************************"
-echo -e "Installing Dotfiles"
-echo "******************************************************************************"
-sleep 5
-
-#!/bin/bash
-cd "$(dirname "$0")"
-#install dotfiles as well
+# Install dotfiles
+echo -e "\033[1;31m Install dotfiles \033[0m"
+say "Install dotfiles"
 cp ./dotfiles/.bash_profile ~/.
 cp ./dotfiles/.gitignore_global ~/.
-
-cd ~/
-
-clear
-echo "******************************************************************************"
-echo -e "Installing Open Source softwares"
-echo "******************************************************************************"
-sleep 5
-
-
-echo -e "Installing X-Code"
-xcode-select --install
-
-echo -e "Installing homebrew"
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-clear
-echo "******************************************************************************"
-echo -e "Pulling GIT repositories"
-echo "******************************************************************************"
-sleep 5
-
-#Setup git
+# Go to home directory
+cd || exit
+# Set default shell to bash
+chsh -s /bin/bash
+# Setup git
 git config --global core.editor "nano"
 git config --global user.name "Gaurav"
 git config --global user.email "gg.spark@gmail.com"
 git config --global core.excludesfile ~/.gitignore_global
 
-### Pull sources
-cd $HOME
-# git clone 
+# Install XCode
+echo -e "\033[1;31m Install XCode \033[0m"
+say "Install XCode"
+xcode-select --install
+
+# Install Homebrew
+echo -e "\033[1;31m Install Homebrew \033[0m"
+say "Install Homebrew"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Install Node
+echo -e "\033[1;31m Install Node \033[0m"
+say "Install Node"
+brew install node
+
+# Install Java
+
+# Install Android
+
+# Install Flutter
+
+# Install Firebase
+
+# Install Gcloud
+
+
+
+
+
+
+# Complete
+echo -e "\033[1;31m \nTime Elapsed: $((SECONDS / 3600)) Hrs, $(((SECONDS / 60) % 60)) Mins, $((SECONDS % 60)) Sec \033[0m"
+echo -e "\033[1;31m \nCompleted \033[0m"
+say "Completed"
