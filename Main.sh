9f64f25b6575d22800519998153b87cde067f633
Gaurav <gg.spark@gmail.com>
2020-07-21 01:54:16 +0530

    Updated vlc which

--- Main.sh
+++ Main.sh
@@ -81,7 +81,9 @@ brew cask install openoffice && brew cask info openoffice
 # Install VLC media player
 echo -e "\033[1;31m Install VLC media player \033[0m"
 say "Install VLC media player"
+which -a vlc && vlc --version
 brew cask install vlc && brew cask info vlc
+which -a vlc && vlc --version
 
 # Install Google Drive
 echo -e "\033[1;31m Install Google Drive \033[0m"
