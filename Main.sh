bfcf3e10f3af564a9262c4a7a1ab260090efe317
Gaurav <gg.spark@gmail.com>
2020-07-20 15:34:25 +0530

    Updated scripts for installation

--- Main.sh
+++ Main.sh
@@ -17,34 +17,74 @@ git config --global user.name "Gaurav"
 git config --global user.email "gg.spark@gmail.com"
 git config --global core.excludesfile ~/.gitignore_global
 
-# Install XCode
-echo -e "\033[1;31m Install XCode \033[0m"
-say "Install XCode"
+# Install XCode CLI Tools
+echo -e "\033[1;31m Install XCode CLI Tools \033[0m"
+say "Install XCode CLI Tools"
+which -a xcode-select; xcode-select --version
 xcode-select --install
+which -a xcode-select; xcode-select --version
 
 # Install Homebrew
 echo -e "\033[1;31m Install Homebrew \033[0m"
 say "Install Homebrew"
+which -a brew; brew --version
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew upgrade
+brew cask upgrade
+which -a brew; brew --version
 
 # Install Node
 echo -e "\033[1;31m Install Node \033[0m"
 say "Install Node"
+which -a node; node --version
+which -a npm; npm --version
 brew install node
-
-# Install Java
+which -a node; node --version
+which -a npm; npm --version
 
 # Install Android
-
-# Install Flutter
+echo -e "\033[1;31m Install Android \033[0m"
+say "Install Android"
+which -a adb; adb --version
+brew install bundletool
+brew cask install android-platform-tools
+brew cask install android-sdk
+brew cask install android-studio
+which -a adb; adb --version
 
 # Install Firebase
+echo -e "\033[1;31m Install Firebase \033[0m"
+say "Install Firebase"
+which -a firebase; firebase --version
+brew install firebase-cli
+which -a firebase; firebase --version
 
 # Install Gcloud
+echo -e "\033[1;31m Install Gcloud \033[0m"
+say "Install Gcloud"
+which -a gcloud; gcloud --version
+brew cask install google-cloud-sdk
+which -a gcloud; gcloud --version
 
+# Install Heroku
+echo -e "\033[1;31m Install Heroku \033[0m"
+say "Install Heroku"
+which -a heroku; heroku --version
+brew tap heroku/brew && brew install heroku
+which -a heroku; heroku --version
 
-
-
+# Install Flutter
+echo -e "\033[1;31m Install Flutter \033[0m"
+say "Install Flutter"
+which -a flutter; flutter --version
+brew cask install flutter
+flutter upgrade
+which -a flutter; flutter --version
+echo -e "\033[1;31m Accept Licenses \033[0m"
+say "Accept Licenses"
+sudo xcodebuild -license
+flutter doctor --android-licenses
+flutter doctor -v
 
 
 # Complete
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # setup_mac
 
-This is a repository containing Mac settings 
+This is a repository containing script for Mac settings 
 
 #### Save the folder in home directory and run
 
@@ -9,22 +9,25 @@ cd $HOME
 git clone https://github.com/ggspark/setup_mac 
 bash ./setup_mac/Main.sh
 ```
+### Software installed by script
+* Bash settings
+* XCode CLI Tools
+* Homebrew
+* Node & npm
+* Android & Android-Studio
+* Firebase CLI
+* GCloud CLI
+* Heroku Toolbelt
+* Flutter
 
-### Macintosh Softwares
+
+### Software to be installed manually
 * Google Chrome
-* Firefox
+* XCode
 * Fliqlo Screensaver
 * Sublime text
 * Open Office
-* Skype
 * VLC media player
-* U torrent
-* Dropbox
 * Google Drive
-* Oracle Virtual box
-* XCode
-* Java SE JDK
-* Android File Transfer
-* Android Studio
 * Intellij Idea
-* Heroku toolbelt
+* Tor Browser
